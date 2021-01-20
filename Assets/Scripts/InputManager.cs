93d971a00fa5c5cb62ce0ec5c9701d5c7728e7c9
Vandit Narain Tyagi <vandit30@gmail.com>
2021-01-20 19:37:50 +0530

    Revert "test revert"
    This reverts commit 45d3704a03a669b9334c1a7a17117be6a899a25b.

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -11,7 +11,7 @@ public class InputManager : MonoSingleton<InputManager>
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         if (Input.GetKey(KeyCode.Mouse0))
         {

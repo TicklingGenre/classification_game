45d3704a03a669b9334c1a7a17117be6a899a25b
Vandit Narain Tyagi <vandit30@gmail.com>
2021-01-20 19:37:30 +0530

    test revert

--- Assets/Scripts/Bin.cs
+++ Assets/Scripts/Bin.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class Bin : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public char binColorTag;
+
+    private GameObject _catchObject;
+    private bool dumpable = false;
+    private bool caught = false;
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        _catchObject = other.gameObject;
+        if(_catchObject.tag == "Brick" && _catchObject.GetComponent<Brick>().isPicked)
+        {
+            dumpable = true;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerStay(Collider other)
     {
-        
+        if (dumpable)
+        {
+            if (Input.GetKeyUp(KeyCode.Mouse0))
+            {
+                Dump();
+            }
+        }
+    }
+
+    void Dump()
+    {
+        caught = true;
+    }
+
+    IEnumerator Catch()
+    {
+        Vector3 newPos = new Vector3(transform.position.x, _catchObject.transform.position.y, _catchObject.transform.position.z - 1f);
+        _catchObject.transform.Translate(newPos);
+        yield return new WaitForSeconds(0.1f);
+    }
+
+    private void Update()
+    {
+        if (caught)
+        {
+            StartCoroutine("Catch");
+            Destroy(_catchObject);
+        }
+            
     }
 }
--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -11,7 +11,7 @@ public class InputManager : MonoSingleton<InputManager>
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if (Input.GetKey(KeyCode.Mouse0))
         {
